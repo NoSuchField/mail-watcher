13d46c1ad459595fc76adb9d5b890b1a2a7fb0d8
Leon Li <leon.li@logicsolutions.com.cn>
2020-03-11 13:56:13 +0800

    replace while true loop with system crontab

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 ## Mail Watcher For XFCE Desktop Environment
+
+### config mailbox
+
+```shell
+cp config.json.example config.json
+```
+
+### setup crontab
+
+```shell
+*/5 * * * * /usr/bin/python /opt/matcher/mail-watcher.py
+```
--- mail-watcher.py
+++ mail-watcher.py
@@ -13,30 +13,28 @@ def decode_mime_words(s):
 
 with open("/opt/matcher/config.json", 'r') as f:
     config = json.loads(f.read())
-    while 1 == 1:
-        for c in config:
-            IMAP4_HOST      = c['host']
-            IMAP4_PORT      = c['port']
-            IMAP4_USER      = c['user']
-            IMAP4_PASS      = c['pass']
-            IMAP4_MAILBOX   = c['mailbox']
+    for c in config:
+        IMAP4_HOST      = c['host']
+        IMAP4_PORT      = c['port']
+        IMAP4_USER      = c['user']
+        IMAP4_PASS      = c['pass']
+        IMAP4_MAILBOX   = c['mailbox']
 
-            time.sleep(30)
-            imap = imaplib.IMAP4_SSL(IMAP4_HOST)
-            (retcode, capabilities) = imap.login(IMAP4_USER, IMAP4_PASS)
-            imap.list()
-            imap.select('inbox')
+        imap = imaplib.IMAP4_SSL(IMAP4_HOST)
+        (retcode, capabilities) = imap.login(IMAP4_USER, IMAP4_PASS)
+        imap.list()
+        imap.select('inbox')
 
-            (retcode, messages) = imap.search(None, '(UNSEEN)')
-            if retcode == 'OK':
-                for num in messages[0].split():
-                    typ, data = imap.fetch(num, '(RFC822)')
-                    for response_part in data:
-                        if isinstance(response_part, tuple):
-                            original = email.message_from_bytes(response_part[1])
-                            title = decode_mime_words(original['From'])
-                            brief = decode_mime_words(original['Subject'])
-                            os.system("/usr/bin/notify-send '"+title + "' '"+brief+"' --icon=evolution-mail")
-            imap.close()
-            imap.logout()
-        
+        (retcode, messages) = imap.search(None, '(UNSEEN)')
+        if retcode == 'OK':
+            for num in messages[0].split():
+                typ, data = imap.fetch(num, '(RFC822)')
+                for response_part in data:
+                    if isinstance(response_part, tuple):
+                        original = email.message_from_bytes(response_part[1])
+                        title = decode_mime_words(original['From'])
+                        brief = decode_mime_words(original['Subject'])
+                        os.system("/usr/bin/notify-send '"+title + "' '"+brief+"' --icon=evolution-mail")
+        imap.close()
+        imap.logout()
+    exit()    
